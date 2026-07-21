e2f9846eb02d48d4bd17e7e74fc1c94b60c964ba
Massum <88305290+MesumAbbas51214@users.noreply.github.com>
2026-07-21 16:56:56 +0200

    Import siteMetrics library in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import NovarchLanding from "./components/NovarchLanding";
+import "./lib/siteMetrics";
 
 const BUSINESS = {
   designation: "NOVARCH",
